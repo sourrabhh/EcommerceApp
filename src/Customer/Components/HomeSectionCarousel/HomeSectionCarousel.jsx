a794d7ae5adb5b518fff5890d98eb431d5d4c78c
sourabh <souraabh@yahoo.com>
2023-07-26 10:34:10 +0530

    carousel added

--- src/Customer/Components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/Customer/Components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -9,7 +9,7 @@ const HomeSectionCarousel = () => {
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 5 },
+        1024: { items: 5.5 },
     };
 
     const items = [1,1,1,1,1].map((item) => <HomeSectionCard />)
