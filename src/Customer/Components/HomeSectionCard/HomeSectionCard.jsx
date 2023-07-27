791a6e04aad8970ec628ca1dc660590eba2d3b5f
sourabh <souraabh@yahoo.com>
2023-07-27 23:25:58 +0530

    Data object changes

--- src/Customer/Components/HomeSectionCard/HomeSectionCard.jsx
+++ src/Customer/Components/HomeSectionCard/HomeSectionCard.jsx
@@ -6,7 +6,7 @@ const HomeSectionCard = ({ product }) => {
     <div className= "cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg" > 
         <div className="h-[13rem] w-[10rem]">
             <img className='object-cover object-top w-full h-full' 
-                src= {product.imageurl} alt='' />
+                src= {product.image} alt='' />
         </div>
 
         <div className='p-4 '>
--- src/Customer/Pages/HomePage/HomePage.jsx
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import MainCarousel from '../../Components/HomeCarousel/MainCarousel';
 import HomeSectionCarousel from '../../Components/HomeSectionCarousel/HomeSectionCarousel';
-import { mens_Kurta } from '../../../Data/Mens/mens_Kurta';
+import {menskurta} from "./../../../Data/Mens/menskurta"
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel />
         <div className='space-y-10 py-20 flex-col justify-center px-5 lg:px-10'>
-          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Men's Kurta"}/>
-          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Women's Kurta"}/>
-          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Men's Shoes"}/>
+          <HomeSectionCarousel data = {menskurta} sectionName = {"Men's Kurta"}/>
+          <HomeSectionCarousel data = {menskurta} sectionName = {"Women's Kurta"}/>
+          <HomeSectionCarousel data = {menskurta} sectionName = {"Men's Shoes"}/>
         </div>
     </div>
 
