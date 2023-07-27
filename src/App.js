87da791f2da5653ca2070b67aaa94ab5c11691d5
sourabh <souraabh@yahoo.com>
2023-07-27 22:56:57 +0530

    test data changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './Customer/Components/Footer/Footer';
 import Navigation from './Customer/Components/Navigation/Navigation';
+import Product from './Customer/Components/Product/Product';
 import HomePage from './Customer/Pages/HomePage/HomePage';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="">
       <Navigation />
         <div>
-          <HomePage />
+          {/* <HomePage /> */}
+          <Product />
         </div>
         <Footer />
     </div>
--- src/Customer/Pages/HomePage/HomePage.jsx
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import MainCarousel from '../../Components/HomeCarousel/MainCarousel';
 import HomeSectionCarousel from '../../Components/HomeSectionCarousel/HomeSectionCarousel';
-import { Mens_Kurta } from '../../../Data/Mens/Mens_Kurta';
+import { mens_Kurta } from '../../../Data/Mens/mens_Kurta';
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel />
         <div className='space-y-10 py-20 flex-col justify-center px-5 lg:px-10'>
-          <HomeSectionCarousel data = {Mens_Kurta} sectionName = {"Men's Kurta"}/>
-          <HomeSectionCarousel data = {Mens_Kurta} sectionName = {"Women's Kurta"}/>
-          <HomeSectionCarousel data = {Mens_Kurta} sectionName = {"Men's Shoes"}/>
+          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Men's Kurta"}/>
+          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Women's Kurta"}/>
+          <HomeSectionCarousel data = {mens_Kurta} sectionName = {"Men's Shoes"}/>
         </div>
     </div>
 
