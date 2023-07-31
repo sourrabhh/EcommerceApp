ddbea3aea33760fabd2190e9892cc18a54992f91
sourabh <souraabh@yahoo.com>
2023-07-31 17:10:09 +0530

    app.js test changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Cart from './Customer/Components/Cart/Cart';
 import Footer from './Customer/Components/Footer/Footer';
 import Navigation from './Customer/Components/Navigation/Navigation';
-import Product from './Customer/Components/Product/Product';
-import ProductDetails from './Customer/Components/ProductDetails/ProductDetails';
-import HomePage from './Customer/Pages/HomePage/HomePage';
+// import Product from './Customer/Components/Product/Product';
+// import ProductDetails from './Customer/Components/ProductDetails/ProductDetails';
+// import HomePage from './Customer/Pages/HomePage/HomePage';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <div>
           {/* <HomePage /> */}
           {/* <Product /> */}
-          <ProductDetails />
+          {/* <ProductDetails /> */}
+          <Cart />
         </div>
         <Footer />
     </div>
