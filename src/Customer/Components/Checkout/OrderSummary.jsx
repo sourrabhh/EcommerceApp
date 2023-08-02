1311d028549877bacbcbb438a9fac1e417c1212f
sourabh <souraabh@yahoo.com>
2023-08-02 19:40:37 +0530

    order summary

--- src/Customer/Components/Checkout/OrderSummary.jsx
+++ src/Customer/Components/Checkout/OrderSummary.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import AddressCard from '../AddressCard/AddressCard';
+import CartItem from '../Cart/CartItem';
+import { Button } from '@mui/material';
 
 const OrderSummary = () => {
   return (
-    <div>OrderSummary</div>
+    <div>
+        <div className='p-5 shadow-lg rounded-md border'>
+            <AddressCard />
+        </div>
+
+        <div>
+
+            <div className='lg:grid grid-cols-3  relative'>
+                <div className='col-span-2'>
+                    {[1,1,1,1].map((item) => <CartItem /> )}
+                </div>
+
+                <div className='px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 '>
+                    <div className='border '>
+                        <p className='mt-2 items-center uppercase font-bold opacity-60 pb-2'>Price Details</p>
+                        <hr />
+                        <div className='space-y-3 font-semibold mb-8'>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span className='ml-2'>Price</span>
+                                <span className='mr-2'>₹899</span>
+                            </div>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span className='ml-2'>Discount</span>
+                                <span className='mr-2 text-green-600'>₹899</span>
+                            </div>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span className='ml-2'>Delivery Charge</span>
+                                <span className='mr-2 text-green-600'>Free</span>
+                            </div>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span className='ml-2'>Total Amount</span>
+                                <span className='mr-2 text-green-600'>₹899</span>
+                            </div>
+                        </div>
+
+                        <Button variant='contained' className='w-full mt-8' sx={{px:"2.5rem", py:"0.5rem", bgcolor:"#9155fd" }}>
+                            Check Out
+                        </Button>
+                        
+                    </div>
+                </div>
+                
+            </div>
+       
+        </div>
+    </div>
   )
 }
 
