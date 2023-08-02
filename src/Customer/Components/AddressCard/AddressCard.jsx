aaa9d46bea4d0224de17d3a549189866f1293e80
sourabh <souraabh@yahoo.com>
2023-08-02 19:33:39 +0530

    address card form

---
+++ src/Customer/Components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className='space-y-3 '>
+        <p className=' font-semibold'>Sourabh Shinde</p>
+        <p>Solapur Gokuldham Society, 123456</p>
+          <div className='space-y-1'>
+            <p className='font-semibold'>Phone Number</p>
+            <p>9876543210</p>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddressCard
