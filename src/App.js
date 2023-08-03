f068a3f918d7a76805b2abf9f68df3c3b01ccdd0
sourabh <souraabh@yahoo.com>
2023-08-03 11:37:03 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Cart from './Customer/Components/Cart/Cart';
-import Checkout from './Customer/Components/Checkout/Checkout';
+// import Cart from './Customer/Components/Cart/Cart';
+// import Checkout from './Customer/Components/Checkout/Checkout';
 import Footer from './Customer/Components/Footer/Footer';
 import Navigation from './Customer/Components/Navigation/Navigation';
-import Product from './Customer/Components/Product/Product';
-import ProductDetails from './Customer/Components/ProductDetails/ProductDetails';
-import HomePage from './Customer/Pages/HomePage/HomePage';
+import OrderPage from './Customer/Components/Order/OrderPage';
+// import Product from './Customer/Components/Product/Product';
+// import ProductDetails from './Customer/Components/ProductDetails/ProductDetails';
+// import HomePage from './Customer/Pages/HomePage/HomePage';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           {/* <Product /> */}
           {/* <ProductDetails /> */}
           {/* <Cart /> */}
-          <Checkout />
+          {/* <Checkout /> */}
+          <OrderPage />
         </div>
         <Footer />
     </div>
